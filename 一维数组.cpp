c1a377611dabaee84d908289dfb31467cdf812d3
1916794924-alt <1916794924@qq.com>
2026-07-28 17:20:18 +0800

    Add files via upload

---
+++ 一维数组.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	//元素逆置
+	int arr[100];
+	int c;
+	cout << "请输入要逆置的数的个数" << endl;
+	cin >> c;
+	cout << "请输入要逆置的"<<c<<"个数" << endl;
+	for (int j = 0;j < c;j++) {
+		cin >> arr[j];
+	}
+	for (int i = 0;i < c/2;i++) {
+		int temp = arr[i];
+		arr[i] = arr[c - 1 - i];
+		arr[c - 1 - i] = temp;
+	}
+	cout << "逆置的结果为：" << endl;
+	for (int i = 0;i < c;i++) {
+		cout << arr[i];
+	}
+	cout << endl;
+
+	//冒泡排序
+	//总排序轮数=元素个数-1
+	for (int i = 0;i < c - 1;i++) {
+		//内层循环对比次数=元素个数-当前排序轮数-1
+		for (int j = 0;j < c - 1 - i;j++) {
+			//升序排序
+			if (arr[j] > arr[j + 1]) {
+				int t = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = t;
+			}
+		}
+	}
+	cout<<"冒泡排序结果为" << endl;
+	for (int k = 0;k < c;k++) {
+		cout << arr[k];
+	}
+	return 0;
+}
---
+++ 二维数组.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //二维数组的定义
+    /*
+    1.int arr[2][3]={1,2,3,4,5,6};
+    2.int arr[2][3]={{1,2,3},{4,5,6}};
+    3.int arr[][3]={1,2,3,4,5,6};
+    */
+    int arr[2][3] = { 1,2,3,4,5,6 };
+    
+    for (int i = 0;i < 2;i++) {
+        for (int j = 0;j < 3;j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    //二维数组的名称用途
+    //1.查看占用的内存空间大小
+    cout << "int型二维数组占用空间大小为:"<<sizeof(arr) << endl;
+    cout << "int型二维数组第一行占用空间大小为:" << sizeof(arr[0]) << endl;
+    cout << "int型二维数组第一个元素占用空间大小为:" << sizeof(arr[0][0]) << endl;
+    //二维数组的行数为=sizeof(arr)/sizeof(arr[0])
+    //二维数组的列数为=sizeof(arr[0])/sizeof(arr[0][0])
+    double arr2[2][3] = { 1,2,3,4,5,6 };
+    cout << "double型二维数组占用空间大小为:" << sizeof(arr2) << endl;
+    cout << "double型二维数组第一行占用空间大小为:" << sizeof(arr2[0]) << endl;
+    cout << "double型二维数组第一个元素占用空间大小为:" << sizeof(arr2[0][0]) << endl;
+    //2.可以查看二维数组的首地址
+    cout << "二维数组的首地址是" << arr << endl;            //也可在(int)arr输出地址的十进制数
+    cout << "二维数组第一行的首地址是" << arr[0] << endl;
+    cout << "二维数组第二行的首地址是" << arr[1] << endl;
+
+    system("pause");
+    return 0;
+}
