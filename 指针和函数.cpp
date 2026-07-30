56576526254d6f566b183bf1e61dc7cb0f6477eb
1916794924-alt <1916794924@qq.com>
2026-07-30 14:33:22 +0800

    Add files via upload

---
+++ 指针和函数.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+//值传递
+void swap1(int n1,int n2) {
+	int t = n1;
+	n1 = n2;
+	n2 = t;
+}
+
+//地址传递
+void swap2(int *n1,int *n2) {
+	int t = *n1;
+	*n1 = *n2;
+	*n2 = t;
+}
+
+int main()
+{
+	int a = 999;
+	int b = 666;
+
+	//值传递，不改变实参
+	swap1(a, b);
+	cout << "a = " << a <<"  " << "b = " << b << endl;
+	
+	//地址传递，可以改变实参
+	swap2(&a, &b);
+	cout << "a = " << a << "  " << "b = " << b << endl;
+
+	return 0;
+}
